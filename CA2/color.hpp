3b4e538862d78ceee86d686782dfa0f80b2f52c3
Parisa Yahyapour Fatideh <parisayahyapoor13@gmail.com>
2025-05-20 07:14:28 +0330

    Create color.hpp

---
+++ CA2/color.hpp
@@ -0,0 +1,15 @@
+#ifndef _COLOR_HPP_
+#define _COLOR_HPP_
+
+#include <string>
+
+using namespace std;
+// ANSI escape codes for colors
+const string RED = "\033[31m";
+const string GREEN = "\033[32m";
+const string YELLOW = "\033[33m";
+const string BLUE = "\033[34m";
+const string MAGNETA = "\033[35m";
+const string CYAN = "\033[36m";
+const string RESET = "\033[0m"; // Reset to default color
+#endif
